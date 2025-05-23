7de07130bbc3b16f88051295523508e4534e83db
Mirodil <mirodil.mavlonov.2022@gmail.com>
2025-05-23 15:51:18 +0500

    comititit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import 'react-toastify/dist/ReactToastify.css'
 const App = () => {
   const [formData, setFormData] = useState({
     name: '',
-    phone: ''
+    phone: '',
+    course: ''
   })
 
   const handleChange = (e) => {
@@ -19,16 +20,16 @@ const App = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    
+
     // Validatsiya
-    if (!formData.name.trim() || !formData.phone.trim()) {
-      toast.error('Iltimos, barcha maydonlarni to\'ldiring!')
+    if (!formData.name.trim() || !formData.phone.trim() || !formData.course) {
+      toast.error('Iltimos, barcha maydonlarni to‘ldiring!')
       return
     }
 
     const telegramBotId = '7714726464:AAFmEHotdot0PV_yOztC9WBiNN4NuvdlZt4'
     const chatId = '2116122699'
-    const telegramMessage = `Ismi: ${formData.name}\nTelefon: ${formData.phone}`
+    const telegramMessage = `Ismi: ${formData.name}\nTelefon: ${formData.phone}\nKurs: ${formData.course}`
 
     try {
       const response = await fetch(`https://api.telegram.org/bot${telegramBotId}/sendMessage`, {
@@ -44,7 +45,7 @@ const App = () => {
 
       if (response.ok) {
         toast.success('Xabar muvaffaqiyatli yuborildi!')
-        setFormData({ name: '', phone: '' })
+        setFormData({ name: '', phone: '', course: '' })
       } else {
         throw new Error('Xabar yuborishda xatolik')
       }
@@ -69,7 +70,7 @@ const App = () => {
       />
       <div className="app__content">
         <div className="app__content-left">
-          {/* Bu qismga kontent qo'shishingiz mumkin */}
+          {/* Bu qismga kontent qo‘shishingiz mumkin */}
         </div>
         <div className="app__content-right">
           <form onSubmit={handleSubmit}>
@@ -89,8 +90,16 @@ const App = () => {
               value={formData.phone}
               onChange={handleChange}
             />
+            <select name="course" value={formData.course} onChange={handleChange} required>
+              <option value="">Kursni tanlang</option>
+              <option value="English">Ingliz tili</option>
+              <option value="Russian">Rus tili</option>
+              <option value="Korean">Koreys tili</option>
+              <option value="English Kids">English Kids</option>
+              <option value="SAT">SAT</option>
+            </select>
             <button className='app__content-right__btn' type='submit'>
-              Jo'natish
+              Jo‘natish
             </button>
           </form>
         </div>
